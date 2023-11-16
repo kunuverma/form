a64c935a6f20bdc565c701b46e3d1eb55b09b6c0
kunalverma <vermakunal15092000@gmail.com>
2023-11-16 11:07:28 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./serviceform.html", "./flowerservice.html", "./vesakday.html", "./fifth,html", "./sixth.html", "./seventh.html", "./ninth.html",
+            "./tenth.html", "./eleventh.html", "./twelve.html", "./therteen.html", "./fourteen.html", "./fifteen.html", "./sixteen.html", "./diandeng2023.html",
+            "./ninteen.html", "./FM-21.html", "./FM-22.html", "./FM-23.html", "./FM-24.html", "./FM-25.html", "./FM-42.html", "./FM-27.html",
+            "./FM-53.html", "./FM-72.html", "./ZZG2.html", "./ZZG3.html", "./eight.html"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
